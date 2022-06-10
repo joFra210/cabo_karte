8f8fb5483cd63867409d574527178fa5a2f862e4
Johannes Franze <12949620+joFra210@users.noreply.github.com>
2022-06-10 08:47:39 +0200

    add functionality to round class

--- lib/features/game/domain/round.dart
+++ lib/features/game/domain/round.dart
@@ -1,6 +1,7 @@
 import 'package:cabo_karte/features/score/domain/score.dart';
 
 class Round {
+  int? _id;
   int number;
   Map<String, Score>? playerScores;
 
@@ -8,4 +9,10 @@ class Round {
     required this.number,
     this.playerScores,
   });
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Round{id: $_id, number: $number, playerScores: $playerScores}';
+  }
 }
