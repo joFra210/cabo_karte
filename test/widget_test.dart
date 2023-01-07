8ed80ad3041a1ff853f1bc988e84bbf17f029581
joFra210 <12949620+joFra210@users.noreply.github.com>
2023-01-07 18:19:39 +0100

    update app Icon and app title

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:cabo_karte/features/app/app_component.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
